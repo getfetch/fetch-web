734c02f8c9939a9e4e599c82ea01bd42ff997aa8
cwargula <chris.wargula@gmail.com>
2013-11-02 21:13:23 -0400

    Add dummy api

---
+++ FetchWeb/Controllers/api/DogsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FetchWeb.Controllers.api
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Description { get; set; }
+        public string Organization { get; set; }
+    }
+
+    public class DogsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Browse(int id)
+        {
+            int zip = id;
+
+            Dog Tess = new Dog
+            {
+                Name = "Tess",
+                Breed = "Beagles",
+                Description = "She be KAAAARAAAAZY",
+                Organization = "Beagle Rescue of America"
+            };
+
+            Dog Shannon = new Dog
+            {
+                Name = "Shannon",
+                Breed = "Pembroke Welsh Corgi/Boston Terrier",
+                Description = "Whose the boss? She is.",
+                Organization = "Bob's Breeder"
+            };
+
+            List<Dog> DogList = new List<Dog>();
+            DogList.Add(Tess);
+            DogList.Add(Shannon);
+
+            var response = new
+            {
+                Zip = zip,
+                Dogs = DogList,
+            };
+            return Ok(response);
+        }
+    }
+}
