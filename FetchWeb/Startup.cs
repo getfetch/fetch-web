152614a5d18485192201d1caa8801b2ea707d979
cwargula <chris.wargula@gmail.com>
2013-11-02 20:08:15 -0400

    new proj

---
+++ FetchWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FetchWeb.Startup))]
+namespace FetchWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
